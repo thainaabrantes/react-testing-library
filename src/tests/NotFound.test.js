26a8603d5042c577262b00b7d073629ebf3228ad
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2022-11-22 22:02:08 -0300

    Testes do componente <NotFound />

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Testa o componente <NotFound.js />', () => {
+  it('A página deve conter um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundTitle = screen.getByRole('heading', { name: /page requested not found/i });
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+
+  it('A página deve mostrar a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+
+    const img = screen.getByRole('img', { name: /pikachu crying because the page requested was not found/i });
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
